fd2e8fa7cb2066503a8f2cb07c5e9a4c1b30a00b
AbdullahinurAA <abdullahinur.abdullahi@gmail.com>
2019-07-30 11:00:42 +0300

    declare target html variables for date

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,7 @@ var date = new Date();
 // variable that holds the target output
 // Day
 var dayToday = document.querySelector('#day');
+// Date
+var dateToday = document.querySelector('#date');
+var monthToday = document.querySelector('#month');
+var yearToday = document.querySelector('#year');
